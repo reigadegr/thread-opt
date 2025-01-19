dc49f839c3ed4f1e6fca025d97cb7d753312d684
reigadegr <272268642@qq.com>
2025-01-19 20:47:58 +0800

    Action: add workflow

--- count.sh
+++ count.sh
@@ -1,3 +1,6 @@
 #!//bin/sh
-echo "代码行数: "
+echo "rs代码行数: "
 find ${0%/*}/src -name "*.rs" -exec cat {} \; | wc -l
+
+echo "py代码行数: "
+find ${0%/*}/*.py ${0%/*}/make*/*.py  -name "*.py" -exec cat {} \; | wc -l
