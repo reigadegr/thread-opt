09ea8681dd0b64023ac9baf25adc61b70f551808
reigadegr <272268642@qq.com>
2025-01-20 23:17:48 +0800

    chore: 清理部分声明

--- count.sh
+++ count.sh
@@ -2,5 +2,5 @@
 echo "rs代码行数: "
 find ${0%/*}/src -name "*.rs" -exec cat {} \; | wc -l
 
-echo "py代码行数: "
+echo "shadow的py代码行数: "
 find ${0%/*}/*.py ${0%/*}/make*/*.py  -name "*.py" -exec cat {} \; | wc -l
