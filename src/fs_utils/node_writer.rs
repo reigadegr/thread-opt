d6b9fb22ab4a3e24432b753445293f44f4320881
reigadegr <2722688642@qq.com>
2025-01-22 01:29:35 +0800

    feat: 表达式转为方法调用

--- src/fs_utils/node_writer.rs
+++ src/fs_utils/node_writer.rs
@@ -2,10 +2,7 @@ use super::dir_ctrl::WORK_DIR;
 use anyhow::Result;
 use std::fs;
 pub fn write_node(path: Result<String>, value: &u32) {
-    let path = match path {
-        Ok(path) => path,
-        Err(_) => WORK_DIR.to_string(),
-    };
+    let path = path.unwrap_or_else(|_| WORK_DIR.to_string());
     let node = format!("{}/tasks", path);
     let _ = fs::write(node, value.to_string());
 }
