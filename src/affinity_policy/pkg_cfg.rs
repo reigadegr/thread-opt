506defc9979c131fb91d60eedc92a9b1efc8070f
reigadegr <272268642@qq.com>
2025-01-24 22:20:06 +0800

    chore: 继续改变量名

--- src/affinity_policy/pkg_cfg.rs
+++ src/affinity_policy/pkg_cfg.rs
@@ -1,7 +1,7 @@
 use super::{policy_pubg, policy_unity};
 use libc::pid_t;
 use std::sync::LazyLock;
-const NORMAL_PACKAGE: [&str; 8] = [
+const UNITY_PACKAGE: [&str; 8] = [
     "com.miHoYo.Yuanshen",
     "com.miHoYo.hkrpg",
     "com.tencent.tmgp.sgame",
@@ -18,7 +18,7 @@ type ConfigTuple<'a> = (&'a [&'a str], fn(pid_t, &str));
 pub static PACKAGE_CONFIGS: LazyLock<[ConfigTuple; 2]> = LazyLock::new(|| {
     [
         (
-            &NORMAL_PACKAGE[..],
+            &UNITY_PACKAGE[..],
             policy_unity::start_task as fn(pid_t, &str),
         ),
         (
