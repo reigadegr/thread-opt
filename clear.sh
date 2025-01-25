acdb120d40a1f550aab0d54b9dd8776b95228da5
reigadegr <272268642@qq.com>
2025-01-25 19:21:26 +0800

    chore: 优化sh脚本效率

--- clear.sh
+++ clear.sh
@@ -4,9 +4,11 @@ rm ${0%/*}/*.bak ${0%/*}/.*.bak module/*.bak
 
 rm $(find ./src -name "thread_opt")
 rm ./thread_opt
+
+for i in $(find ./src -name "*.rs"); do
+    nohup dos2unix $i >/dev/null 2>&1 &
+done
+
 uid=$(dumpsys package com.termux | grep appId | awk 'NR==1{print $1}' | cut -d '=' -f2)
 chown -R $uid:$uid  ./src
 chmod -R 0755 ./src
-for i in $(find ./src -name "*.rs"); do
-    dos2unix $i
-done
