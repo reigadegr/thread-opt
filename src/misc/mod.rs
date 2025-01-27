53104c4f7b5b92f3b3db4606d3df6bc3825df95c
reigadegr <272268642@qq.com>
2025-01-27 14:50:11 +0800

    fix: 无法修改线程名，原因是长度大于16

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,13 +1,11 @@
 pub mod logger;
 use super::misc::logger::{init_log, log_metainfo};
 use anyhow::Result;
-use libc::pthread_setname_np;
 use log::info;
-use std::{ffi::CString, process};
 
 pub fn init_misc() {
     working_in_background();
-    let rs = set_main_thread_name();
+    let rs = set_main_thread_name("affinity_opt");
     if rs.is_err() {
         info!("Cannot rename the main thread name.");
     }
@@ -16,15 +14,16 @@ pub fn init_misc() {
 }
 
 fn working_in_background() {
-    let self_pid = process::id();
+    let self_pid = std::process::id();
     let _ = std::fs::write("/dev/cpuset/background/tasks", self_pid.to_string());
 }
 
-fn set_main_thread_name() -> Result<()> {
-    let thread_name = CString::new("affinity_optimizer")?;
+fn set_main_thread_name(name: &str) -> Result<()> {
+    let truncated_name = if name.len() > 15 { &name[..15] } else { name };
 
+    let thread_name = std::ffi::CString::new(truncated_name)?;
     unsafe {
-        pthread_setname_np(libc::pthread_self(), thread_name.as_ptr());
+        libc::pthread_setname_np(libc::pthread_self(), thread_name.as_ptr());
     }
     Ok(())
 }
