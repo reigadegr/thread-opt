8e85c46044966839c02360014004fd1ccf6e36a1
reigadegr <272268642@qq.com>
2025-01-27 13:05:02 +0800

    chore: 修改主线程命名

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,9 +1,16 @@
 pub mod logger;
 use super::misc::logger::{init_log, log_metainfo};
-use std::process;
+use anyhow::Result;
+use libc::pthread_setname_np;
+use log::info;
+use std::{ffi::CString, process};
 
 pub fn init_misc() {
     working_in_background();
+    let rs = set_main_thread_name();
+    if rs.is_err() {
+        info!("Cannot rename the main thread name.");
+    }
     let _ = init_log();
     log_metainfo();
 }
@@ -11,4 +18,13 @@ pub fn init_misc() {
 fn working_in_background() {
     let self_pid = process::id();
     let _ = std::fs::write("/dev/cpuset/background/tasks", self_pid.to_string());
+}
+
+fn set_main_thread_name() -> Result<()> {
+    let thread_name = CString::new("affinity_optimizer")?;
+
+    unsafe {
+        pthread_setname_np(libc::pthread_self(), thread_name.as_ptr());
+    }
+    Ok(())
 }
