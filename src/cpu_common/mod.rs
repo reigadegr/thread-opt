f9f1299e89d0a3f8ffb6a635d524425e09253043
reigadegr <272268642@qq.com>
2025-01-30 21:59:32 +0800

    feat: 删除无用字段

--- src/cpu_common/mod.rs
+++ src/cpu_common/mod.rs
@@ -5,7 +5,6 @@ use process_monitor::ProcessMonitor;
 #[derive(Debug)]
 pub struct Controller {
     process_monitor: ProcessMonitor,
-    util_max: Option<f64>,
     max_tid: Option<pid_t>, // 新增字段存储tid
 }
 
@@ -13,24 +12,22 @@ impl Controller {
     pub fn new() -> Self {
         Self {
             process_monitor: ProcessMonitor::new(),
-            util_max: None,
             max_tid: None,
         }
     }
 
     pub fn init_game(&mut self, pid: pid_t) {
         self.process_monitor.set_pid(Some(pid));
-        self.util_max = None;
+        self.max_tid = None;
     }
 
     pub fn init_default(&mut self) {
         self.process_monitor.set_pid(None);
-        self.util_max = None;
+        self.max_tid = None;
     }
 
     pub fn update_util_max(&mut self) {
         if let Some((util, tid)) = self.process_monitor.update_util_max() {
-            self.util_max = Some(util);
             self.max_tid = Some(tid);
         }
     }
@@ -38,9 +35,5 @@ impl Controller {
     // 新增获取tid的方法
     pub const fn max_tid(&self) -> Option<pid_t> {
         self.max_tid
-    }
-
-    pub fn util_max(&self) -> f64 {
-        self.util_max.unwrap_or_default()
     }
 }
