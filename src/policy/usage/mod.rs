a782cf29d97045908db682d36948483e953ff66e
reigadegr <272268642@qq.com>
2025-02-03 12:52:33 +0800

    feat: 使用HashSet优化效率，游戏退出时若被划掉后台不再读目录和绑核

--- src/policy/usage/mod.rs
+++ src/policy/usage/mod.rs
@@ -1,24 +1,19 @@
 use compact_str::CompactString;
 use flume::{bounded, Receiver, Sender};
-use hashbrown::HashMap;
+use hashbrown::{HashMap, HashSet};
 use libc::pid_t;
-#[cfg(debug_assertions)]
-use log::debug;
 use once_cell::sync::Lazy;
 
 pub mod usage_top1;
 pub mod usage_top2;
 
 // 定义别名
-type ChannelType = (Sender<Vec<pid_t>>, Receiver<Vec<pid_t>>);
+type ChannelType = (Sender<HashSet<pid_t>>, Receiver<HashSet<pid_t>>);
 
 // // 使用别名定义全局变量
 pub static UNNAME_TIDS: Lazy<ChannelType> = Lazy::new(|| bounded(0));
 
-fn get_thread_tids(task_map: &HashMap<pid_t, CompactString>, prefix: &str) -> Vec<pid_t> {
-    #[cfg(debug_assertions)]
-    debug!("原始的task_map:{task_map:?}");
-
+fn get_thread_tids(task_map: &HashMap<pid_t, CompactString>, prefix: &str) -> HashSet<pid_t> {
     task_map
         .iter()
         .filter(|(_, name)| name.starts_with(prefix))
