5f4e82906410d526d3d9e1751e3db882c9a22812
reigadegr <272268642@qq.com>
2025-02-04 15:54:44 +0800

    chore: 取消部分clippy的允许规则，取消强制内联函数

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 #![deny(clippy::pedantic)]
 #![warn(clippy::nursery)]
 #![allow(
-    clippy::module_name_repetitions,
-    clippy::cast_possible_truncation,
-    clippy::cast_sign_loss,
     clippy::cast_precision_loss,
-    clippy::cast_possible_wrap,
     clippy::non_std_lazy_statics
 )]
 
