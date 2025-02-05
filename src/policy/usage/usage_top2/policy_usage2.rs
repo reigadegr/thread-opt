59b0b19e27283fd1ae7009b1fe209bfcf7586ab4
reigadegr <272268642@qq.com>
2025-02-05 16:50:41 +0800

    feat: 在读取俩最高负载的过程中，通过统计高负载的线程频次来确定重负载线程，并记录，以便后续降低开销避免绑错

--- src/policy/usage/usage_top2/policy_usage2.rs
+++ src/policy/usage/usage_top2/policy_usage2.rs
@@ -3,6 +3,7 @@ use crate::policy::{
     pkg_cfg::StartArgs,
     usage::{get_thread_tids, UNNAME_TIDS},
 };
+use hashbrown::HashMap;
 #[cfg(debug_assertions)]
 use log::debug;
 use std::time::Duration;
@@ -11,6 +12,13 @@ pub fn start_task(args: &mut StartArgs) {
     // 获取全局通道的发送端
     let tx = &UNNAME_TIDS.0;
     args.controller.init_game(*args.pid);
+    // 创建一个容量为20的Vec<pid_t>
+    let mut high_usage_tids = Vec::with_capacity(20);
+
+    let mut finish = false;
+
+    let mut usage_top1 = 0;
+    let mut usage_top2 = 0;
 
     loop {
         let pid = args.activity_utils.top_app_utils.get_pid();
@@ -21,31 +29,68 @@ pub fn start_task(args: &mut StartArgs) {
 
         let task_map = args.activity_utils.tid_utils.get_task_map(*pid);
 
-        let unname_tids = get_thread_tids(task_map, b"Thread-");
-        #[cfg(debug_assertions)]
-        debug!("发送即将开始");
-        tx.send(unname_tids).unwrap();
-        #[cfg(debug_assertions)]
-        debug!("发送已经完毕，喵等待一段时间计算");
-        std::thread::sleep(Duration::from_millis(100));
-        args.controller.update_max_usage_tid();
-        let Some(tid1) = args.controller.first_max_tid() else {
+        if finish {
+            execute_policy(task_map, usage_top1, usage_top2);
+            std::thread::sleep(Duration::from_millis(2000));
+        } else {
+            let unname_tids = get_thread_tids(task_map, b"Thread-");
             #[cfg(debug_assertions)]
-            debug!("获取不到first max tid，直接循环");
-            std::thread::sleep(Duration::from_millis(100));
-            continue;
-        };
-
-        let Some(tid2) = args.controller.second_max_tid() else {
+            debug!("发送即将开始");
+            tx.send(unname_tids).unwrap();
             #[cfg(debug_assertions)]
-            debug!("获取不到second max tid，直接循环");
+            debug!("发送已经完毕，喵等待一段时间计算");
             std::thread::sleep(Duration::from_millis(100));
-            continue;
-        };
+            args.controller.update_max_usage_tid();
+            let Some(tid1) = args.controller.first_max_tid() else {
+                #[cfg(debug_assertions)]
+                debug!("获取不到first max tid，直接循环");
+                std::thread::sleep(Duration::from_millis(100));
+                continue;
+            };
 
-        #[cfg(debug_assertions)]
-        debug!("负载第一高:{tid1}\n第二高:{tid2}");
-        execute_policy(task_map, tid1, tid2);
+            let Some(tid2) = args.controller.second_max_tid() else {
+                #[cfg(debug_assertions)]
+                debug!("获取不到second max tid，直接循环");
+                std::thread::sleep(Duration::from_millis(100));
+                continue;
+            };
+
+            if high_usage_tids.len() < 20 {
+                high_usage_tids.push(tid1);
+                high_usage_tids.push(tid2);
+            } else {
+                args.controller.init_default();
+
+                let mut tid_counts = HashMap::new();
+                for &tid in &high_usage_tids {
+                    *tid_counts.entry(tid).or_insert(0) += 1;
+                }
+
+                // 按频次排序，取出频次最高的两个tid
+                let mut sorted_tids: Vec<_> = tid_counts.into_iter().collect();
+                sorted_tids.sort_by(|a, b| b.1.cmp(&a.1));
+                let (sort1, sort2) = if let Some((tid1, _)) = sorted_tids.first() {
+                    if let Some((tid2, _)) = sorted_tids.get(1) {
+                        (*tid1, *tid2)
+                    } else {
+                        (*tid1, *tid1)
+                    }
+                } else {
+                    (0, 0)
+                };
+                finish = true;
+                usage_top1 = sort1;
+                usage_top2 = sort2;
+                #[cfg(debug_assertions)]
+                debug!("计算后最终结果为:{usage_top1}\n第二高:{usage_top2}");
+                continue;
+            }
+
+            #[cfg(debug_assertions)]
+            debug!("负载第一高:{tid1}\n第二高:{tid2}");
+            execute_policy(task_map, tid1, tid2);
+        }
+
         std::thread::sleep(Duration::from_millis(1900));
     }
 }
