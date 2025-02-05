8442d0ced9d28d48d5455fdd49ed2224ae7319b6
reigadegr <272268642@qq.com>
2025-02-05 17:21:32 +0800

    feat: 只需要获取一个最高负载的策略也加入记录高负载线程频次的功能

--- src/policy/usage/usage_top1/policy_ue_top1.rs
+++ src/policy/usage/usage_top1/policy_ue_top1.rs
@@ -3,6 +3,7 @@ use crate::policy::{
     pkg_cfg::StartArgs,
     usage::{get_thread_tids, UNNAME_TIDS},
 };
+use hashbrown::HashMap;
 #[cfg(debug_assertions)]
 use log::debug;
 use std::time::Duration;
@@ -17,6 +18,12 @@ pub fn start_task(args: &mut StartArgs) {
     // 获取全局通道的发送端
     let tx = &UNNAME_TIDS.0;
     args.controller.init_game(*args.pid);
+    // 创建一个容量为20的Vec<pid_t>
+    let mut high_usage_tids = Vec::with_capacity(10);
+
+    let mut finish = false;
+
+    let mut usage_top1 = 0;
 
     loop {
         let pid = args.activity_utils.top_app_utils.get_pid();
@@ -26,21 +33,52 @@ pub fn start_task(args: &mut StartArgs) {
         }
 
         let task_map = args.activity_utils.tid_utils.get_task_map(*pid);
-
-        let unname_tids = get_thread_tids(task_map, b"Thread-");
-        #[cfg(debug_assertions)]
-        debug!("发送即将开始");
-        tx.send(unname_tids).unwrap();
-        #[cfg(debug_assertions)]
-        debug!("发送已经完毕");
-        std::thread::sleep(Duration::from_millis(100));
-        args.controller.update_max_usage_tid();
-        let Some(tid1) = args.controller.first_max_tid() else {
+        if finish {
+            Policy::new(&TOP, &ONLY6, &ONLY7, &MIDDLE, &BACKEND)
+                .execute_policy(task_map, usage_top1);
             std::thread::sleep(Duration::from_millis(100));
-            continue;
-        };
+        } else {
+            let unname_tids = get_thread_tids(task_map, b"Thread-");
+            #[cfg(debug_assertions)]
+            debug!("发送即将开始");
+            tx.send(unname_tids).unwrap();
+            #[cfg(debug_assertions)]
+            debug!("发送已经完毕");
+            std::thread::sleep(Duration::from_millis(100));
+            args.controller.update_max_usage_tid();
+            let Some(tid1) = args.controller.first_max_tid() else {
+                std::thread::sleep(Duration::from_millis(100));
+                continue;
+            };
 
-        Policy::new(&TOP, &ONLY6, &ONLY7, &MIDDLE, &BACKEND).execute_policy(task_map, tid1);
+            if high_usage_tids.len() < 10 {
+                high_usage_tids.push(tid1);
+                #[cfg(debug_assertions)]
+                debug!("负载第一高:{tid1}\n");
+                Policy::new(&TOP, &ONLY6, &ONLY7, &MIDDLE, &BACKEND).execute_policy(task_map, tid1);
+            } else {
+                args.controller.init_default();
+                let mut tid_counts = HashMap::new();
+                for &tid in &high_usage_tids {
+                    *tid_counts.entry(tid).or_insert(0) += 1;
+                }
+
+                // 按频次排序，取出频次最高的一个tid
+                let mut sorted_tids: Vec<_> = tid_counts.into_iter().collect();
+
+                sorted_tids.sort_by(|a, b| b.1.cmp(&a.1));
+                if let Some((sort1, _)) = sorted_tids.first() {
+                    usage_top1 = *sort1;
+                }
+
+                finish = true;
+
+                high_usage_tids.clear();
+                #[cfg(debug_assertions)]
+                debug!("计算后最终结果为:{usage_top1}\n");
+                continue;
+            }
+        }
 
         std::thread::sleep(Duration::from_millis(1900));
     }
