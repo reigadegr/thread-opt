ee4fa50c456b5257dadc3163b3185c4aa701dc0a
reigadegr <272268642@qq.com>
2025-02-07 13:09:29 +0800

    chore: 更新模块版本和安装脚本打印描述

--- module/customize.sh
+++ module/customize.sh
@@ -1,2 +1,6 @@
 #!/system/bin/sh
 echo "Hello World!"
+echo "仓库地址: https://github.com/reigadegr/thread-opt"
+echo "适配游戏请截屏Scene帧率统计图 && 线程负载统计图"
+echo "同时附上包名提交到仓库的issue处"
+echo "祝使用愉快，觉得好用还请辛苦您给仓库点个Star，会支持作者写出更好的东西"
