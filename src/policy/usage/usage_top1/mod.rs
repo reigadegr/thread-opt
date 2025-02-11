bc62b3562550dfd5652f9b3a517b18c54dbe0956
reigadegr <272268642@qq.com>
2025-02-11 13:35:52 +0800

    refactor: 写了一半的宏

--- src/policy/usage/usage_top1/mod.rs
+++ src/policy/usage/usage_top1/mod.rs
@@ -5,47 +5,74 @@ pub mod unnamed;
 
 macro_rules! top1_policy {
     ($Top: ident, $Only6: ident, $Only7: ident, $Middle: ident, $Backend: ident,$CommPrefix:expr) => {
-        pub fn start_task(args: &mut StartArgs) {
-            args.controller.init_game(true);
-            // 获取全局通道的发送端
-            let tx = &UNNAME_TIDS.0;
+        use flume::Sender;
+        use libc::pid_t;
 
-            let mut finish = false;
-            let mut usage_top1 = 0;
+        struct StartTask<'b, 'a: 'b> {
+            args: &'b mut StartArgs<'a>,
+            tx: &'b Sender<Vec<pid_t>>,
+            usage_top1: pid_t,
+            finish: bool,
+        }
 
-            loop {
-                let pid = args.activity_utils.top_app_utils.get_pid();
-                if unlikely(pid != args.pid) {
-                    args.controller.init_default();
-                    return;
+        impl<'b, 'a: 'b> StartTask<'b, 'a> {
+            fn new(start_args: &'b mut StartArgs<'a>) -> Self {
+                Self {
+                    args: start_args,
+                    tx: &UNNAME_TIDS.0,
+                    usage_top1: 0,
+                    finish: false,
                 }
+            }
 
-                let task_map = args.activity_utils.tid_utils.get_task_map(pid);
-                if likely(finish) {
-                    Policy::new(&$Top, &$Only6, &$Only7, &$Middle, &$Backend)
-                        .execute_policy(task_map, usage_top1);
-                    std::thread::sleep(Duration::from_millis(1000));
-                } else {
-                    let unname_tids = get_thread_tids(task_map, $CommPrefix);
-                    #[cfg(debug_assertions)]
-                    debug!("发送即将开始");
-                    tx.send(unname_tids).unwrap();
-                    #[cfg(debug_assertions)]
-                    debug!("发送已经完毕");
-                    std::thread::sleep(Duration::from_millis(100));
-                    args.controller.update_max_usage_tid();
-                    check_some! {tid1, args.controller.first_max_tid(), "无法获取最大负载tid"};
-                    usage_top1 = tid1;
-                    args.controller.init_default();
-                    finish = true;
-                    #[cfg(debug_assertions)]
-                    debug!("计算后最终结果为:{usage_top1}\n");
-                    continue;
-                }
-
+            fn after_usage_task(&mut self) {
+                let task_map = self
+                    .args
+                    .activity_utils
+                    .tid_utils
+                    .get_task_map(self.args.pid);
+                Policy::new(&$Top, &$Only6, &$Only7, &$Middle, &$Backend).execute_policy(task_map, self.usage_top1);
                 std::thread::sleep(Duration::from_millis(1000));
             }
+
+            fn start_task(&mut self) {
+                loop {
+                    let pid = self.args.activity_utils.top_app_utils.get_pid();
+                    if unlikely(pid != self.args.pid) {
+                        self.args.controller.init_default();
+                        return;
+                    }
+
+                    let task_map = self.args.activity_utils.tid_utils.get_task_map(pid);
+                    if likely(self.finish) {
+                        self.after_usage_task();
+                    } else {
+                        let unname_tids = get_thread_tids(task_map, $CommPrefix);
+                        #[cfg(debug_assertions)]
+                        debug!("发送即将开始");
+                        self.tx.send(unname_tids).unwrap();
+                        #[cfg(debug_assertions)]
+                        debug!("发送已经完毕");
+                        std::thread::sleep(Duration::from_millis(100));
+                        self.args.controller.update_max_usage_tid();
+                        check_some! {tid1, self.args.controller.first_max_tid(), "无法获取最大负载tid"};
+                        self.usage_top1 = tid1;
+                        self.args.controller.init_default();
+                        self.finish = true;
+                        #[cfg(debug_assertions)]
+                        debug!("计算后最终结果为:{0}\n", self.usage_top1);
+                        continue;
+                    }
+
+                    std::thread::sleep(Duration::from_millis(1000));
+                }
+            }
         }
+
+        pub fn start_task(args: &mut StartArgs<'_>) {
+            StartTask::new(args).start_task();
+        }
+
     };
 }
 
