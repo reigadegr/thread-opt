30e6e82a2fa068bde9131455924ec3079d24212c
reigadegr <272268642@qq.com>
2025-02-12 21:15:17 +0800

    lint : clippy fix && 清理冗余宏

--- src/policy/name_match/mod.rs
+++ src/policy/name_match/mod.rs
@@ -31,7 +31,7 @@ impl<'b, 'a: 'b> StartTask<'b, 'a> {
             #[cfg(debug_assertions)]
             let start = std::time::Instant::now();
             let task_map = self.args.activity_utils.tid_utils.get_task_map(pid);
-            common::Policy::new(&self.policy).execute_policy(task_map);
+            common::Policy::new(self.policy).execute_policy(task_map);
             #[cfg(debug_assertions)]
             {
                 let end = start.elapsed();
@@ -45,38 +45,8 @@ impl<'b, 'a: 'b> StartTask<'b, 'a> {
         }
     }
 }
-// 定义宏，但不导出
-macro_rules! name_match_policy {
-    ($top:expr, $only6:expr, $only7:expr, $middle:expr, $background:expr) => {
-        use likely_stable::unlikely;
-        #[cfg(debug_assertions)]
-        use log::debug;
-        pub fn start_task(args: &mut $crate::policy::pkg_cfg::StartArgs) {
-            loop {
-                let pid = args.activity_utils.top_app_utils.get_pid();
-                if unlikely(pid != args.pid) {
-                    return;
-                }
-                #[cfg(debug_assertions)]
-                let start = std::time::Instant::now();
-                let task_map = args.activity_utils.tid_utils.get_task_map(pid);
-                super::common::Policy::new($top, $only6, $only7, $middle, $background)
-                    .execute_policy(task_map);
-                #[cfg(debug_assertions)]
-                {
-                    let end = start.elapsed();
-                    debug!(
-                        "单线程:一轮绑定核心完成时间: {:?} 数组长度{}",
-                        end,
-                        task_map.len()
-                    );
-                }
-                std::thread::sleep(std::time::Duration::from_millis(2000));
-            }
-        }
-    };
-}
 
+// 定义宏，但不导出
 macro_rules! name_match_init {
     () => {
         use super::common::Policy;
@@ -94,7 +64,5 @@ macro_rules! name_match_init {
     };
 }
 
-use name_match_init;
-
 // 重新导出宏，使其在子模块中可用
-use name_match_policy;
+use name_match_init;
