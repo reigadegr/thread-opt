850433bf76d74481862e2b233d706278ff5a0be9
reigadegr <272268642@qq.com>
2025-02-14 01:50:45 +0800

    fix: 蛋仔派对跟燕云十六声策略写反

--- src/policy/usage/usage_top2/policy_party.rs
+++ src/policy/usage/usage_top2/policy_party.rs
@@ -1,5 +1,5 @@
 use crate::policy::pkg_cfg::StartArgs;
 
 pub fn start_task(args: &mut StartArgs<'_>) {
-    super::StartTask::new(args).start_task(b"Thread-", None);
+    super::StartTask::new(args).start_task(b"Thread-", Some(b"MainThread"));
 }
