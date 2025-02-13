9848795887be7c5e2ab4cc9cd71f4b38bc190d5d
reigadegr <272268642@qq.com>
2025-02-14 03:45:07 +0800

    refactor: 优雅的重构局部代码

--- src/policy/usage/usage_top2/mod.rs
+++ src/policy/usage/usage_top2/mod.rs
@@ -36,13 +36,13 @@ impl<'b, 'a: 'b> StartTask<'b, 'a> {
         execute_policy(task_map, tid1, tid2);
     }
 
-    fn update_tids(&mut self, comm_prefix1: &[u8]) {
+    fn update_tids(&mut self, comm_prefix: &[u8]) {
         let task_map = self
             .args
             .activity_utils
             .tid_utils
             .get_task_map(self.args.pid);
-        let unname_tids = get_thread_tids(task_map, comm_prefix1);
+        let unname_tids = get_thread_tids(task_map, comm_prefix);
 
         #[cfg(debug_assertions)]
         debug!("发送即将开始");
@@ -64,17 +64,16 @@ impl<'b, 'a: 'b> StartTask<'b, 'a> {
             }
 
             self.update_tids(comm_prefix1);
-            check_some! {tid1, self.args.controller.first_max_tid(), "无法获取最大负载tid"};
-            check_some! {tid2, self.args.controller.second_max_tid(), "无法获取第二负载tid"};
-
-            if let Some(prefix2) = comm_prefix2 {
+            check_some! { tid1, self.args.controller.first_max_tid(), "无法获取最大负载tid" };
+            
+            let tid2 = if let Some(prefix2) = comm_prefix2 {
                 self.update_tids(prefix2);
-                check_some! {tid2, self.args.controller.first_max_tid(), "无法获取最大负载tid"};
-                #[cfg(debug_assertions)]
-                debug!("负载第一高:{tid1}\n第二高:{tid2}");
-                self.bind_tids(tid1, tid2);
-                continue;
-            }
+                check_some! { tid2, self.args.controller.first_max_tid(), "无法获取最大负载tid" };
+                tid2
+            } else {
+                check_some! { tid2, self.args.controller.second_max_tid(), "无法获取第二负载tid" };
+                tid2
+            };
 
             #[cfg(debug_assertions)]
             debug!("负载第一高:{tid1}\n第二高:{tid2}");
