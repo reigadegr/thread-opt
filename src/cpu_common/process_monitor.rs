01708d91f901a7401e3d59d622c27999a9fbc4aa
reigadegr <272268642@qq.com>
2025-02-14 22:51:35 +0800

    refactor: 提前获取cputime，不批量传递UsageTracker对象

--- src/cpu_common/process_monitor.rs
+++ src/cpu_common/process_monitor.rs
@@ -4,24 +4,23 @@ use hashbrown::HashMap;
 use libc::pid_t;
 
 pub fn get_high_usage_tids(target_tids: &[pid_t]) -> (pid_t, pid_t) {
-    let all_trackers: HashMap<pid_t, UsageTracker> = target_tids
+    let all_trackers: HashMap<pid_t, u64> = target_tids
         .iter()
         .copied()
-        .map(|tid| (tid, UsageTracker::new(tid)))
+        .map(|tid| (tid, UsageTracker::new(tid).try_calculate()))
         .collect();
 
     let (tid1, tid2) = get_top_usage_tid(&all_trackers);
     (tid1, tid2)
 }
 
-fn get_top_usage_tid(trackers: &HashMap<pid_t, UsageTracker>) -> (pid_t, pid_t) {
+fn get_top_usage_tid(trackers: &HashMap<pid_t, u64>) -> (pid_t, pid_t) {
     let mut tid1 = -1;
     let mut tid2 = -1;
     let mut usage1: u64 = 0;
     let mut usage2: u64 = 0;
 
-    for (tid, tracker) in trackers {
-        let cputime = tracker.try_calculate();
+    for (tid, &cputime) in trackers {
         if cputime > usage1 {
             // 避免极端情况下获取到的cputime永远比上一个大导致tid2不被赋值
             usage2 = usage1;
