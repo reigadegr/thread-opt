0b9e11c86b107c665d17db65e8258b06d9e58343
reigadegr <272268642@qq.com>
2025-02-14 23:00:46 +0800

    refactor: 循环中另一个字段也改成&tid，保持风格一致

--- src/cpu_common/process_monitor.rs
+++ src/cpu_common/process_monitor.rs
@@ -20,20 +20,20 @@ fn get_top_usage_tid(trackers: &HashMap<pid_t, u64>) -> (pid_t, pid_t) {
     let mut usage1: u64 = 0;
     let mut usage2: u64 = 0;
 
-    for (tid, &cputime) in trackers {
+    for (&tid, &cputime) in trackers {
         if cputime > usage1 {
             // 避免极端情况下获取到的cputime永远比上一个大导致tid2不被赋值
             usage2 = usage1;
             tid2 = tid1;
 
             usage1 = cputime;
-            tid1 = *tid;
+            tid1 = tid;
             continue;
         }
 
         if cputime > usage2 {
             usage2 = cputime;
-            tid2 = *tid;
+            tid2 = tid;
         }
     }
     (tid1, tid2)
