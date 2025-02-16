628a6ea9fc853bbc542e297dccb714ee34c1986a
reigadegr <272268642@qq.com>
2025-02-16 12:41:52 +0800

    feat: 适配codm

--- src/policy/usage/usage_top1/policies/mod.rs
+++ src/policy/usage/usage_top1/policies/mod.rs
@@ -1,4 +1,5 @@
 pub mod policy_cocos;
+pub mod policy_codm;
 pub mod policy_second;
 pub mod policy_top1;
 pub mod policy_ue5;
--- src/utils/affinity_setter.rs
+++ src/utils/affinity_setter.rs
@@ -3,7 +3,7 @@ use libc::{CPU_SET, CPU_ZERO, cpu_set_t, pid_t, sched_setaffinity};
 // 辅助通用函数：初始化 CPU 集
 pub fn create_cpu_set(cpu_indices: &[u8]) -> cpu_set_t {
     unsafe {
-        let mut cpu_set = std::mem::zeroed::<cpu_set_t>();
+        let mut cpu_set = std::mem::zeroed();
         CPU_ZERO(&mut cpu_set);
         for &cpu_index in cpu_indices {
             CPU_SET(cpu_index as _, &mut cpu_set);
