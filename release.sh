4a16c5c65742e70f907c4dec176a0e775fd5b5ed
reigadegr <272268642@qq.com>
2025-02-18 23:05:31 +0800

    feat: 引入mimalloc，同时优化构建脚本(使用mimalloc不能-Z build-std且使用mold，否则项目爆炸)

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@ rm -rf output
 rm -rf $(find ./target -name "*thread-opt*")
 rm -rf $(find ./target -name "*thread_opt*")
 cargo fmt
-python3 ./make.py build --release --nightly
+RUSTFLAGS="-C default-linker-libraries" python3 ./make.py build --release --nightly
