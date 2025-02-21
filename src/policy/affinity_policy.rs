a03c37a7b425418f38c7695bed2dcd35fe2009c1
reigadegr <272268642@qq.com>
2025-02-21 17:47:39 +0800

    feat: once_cell的Lazy存储具体要调用的函数，避免运行时每次判断开销过高

--- src/policy/affinity_policy.rs
+++ src/policy/affinity_policy.rs
@@ -6,28 +6,38 @@ use crate::{
     },
 };
 use libc::pid_t;
+use once_cell::sync::Lazy;
+
+static ONLY6_POLICY_FN: Lazy<fn(pid_t)> = Lazy::new(|| {
+    if get_middle_group() == get_background_group() {
+        return bind_tid_to_only6;
+    }
+    bind_tid_to_middle
+});
+
+static MODDLE_POLICY_FN: Lazy<fn(pid_t)> = Lazy::new(|| {
+    if get_top_group().len() == 4 {
+        return bind_tid_to_top;
+    }
+    bind_tid_to_middle
+});
 
 pub fn top_policy(tid: pid_t) {
     bind_tid_to_top(tid);
 }
 
 pub fn only6_policy(tid: pid_t) {
-    if get_middle_group() == get_background_group() {
-        bind_tid_to_only6(tid);
-        return;
-    }
-    bind_tid_to_middle(tid);
+    ONLY6_POLICY_FN(tid);
 }
+
 pub fn only7_policy(tid: pid_t) {
     bind_tid_to_only7(tid);
 }
+
 pub fn middle_policy(tid: pid_t) {
-    if get_top_group().len() == 4 {
-        bind_tid_to_top(tid);
-        return;
-    }
-    bind_tid_to_middle(tid);
+    MODDLE_POLICY_FN(tid);
 }
+
 pub fn background_policy(tid: pid_t) {
     bind_tid_to_background(tid);
 }
