cdab34d4b78c21b65a570ccfbc27645146ebae79
reigadegr <272268642@qq.com>
2025-02-24 17:43:28 +0800

    feat: 给读文件实现一个guard

--- src/utils/guard.rs
+++ src/utils/guard.rs
@@ -1,5 +1,6 @@
+use anyhow::{Result, anyhow};
 use core::ptr::NonNull;
-use libc::{DIR, closedir};
+use libc::{DIR, c_int, close, closedir};
 
 pub struct DirGuard(Option<NonNull<DIR>>);
 
@@ -8,16 +9,33 @@ impl DirGuard {
     pub const fn new(dir: *mut DIR) -> Self {
         Self(NonNull::new(dir))
     }
+}
+
+impl Drop for DirGuard {
     /// 关闭目录并释放资源。
-    pub fn close(&mut self) {
+    fn drop(&mut self) {
         if let Some(dir) = self.0.take() {
             let _ = unsafe { closedir(dir.as_ptr()) };
         }
     }
 }
 
-impl Drop for DirGuard {
+pub struct FileGuard(c_int);
+
+impl FileGuard {
+    pub fn new(fd: c_int) -> Result<Self> {
+        if fd == -1 {
+            Err(anyhow!("Cannot open file."))
+        } else {
+            Ok(Self(fd))
+        }
+    }
+}
+
+impl Drop for FileGuard {
     fn drop(&mut self) {
-        self.close();
+        if self.0 != -1 {
+            unsafe { close(self.0) };
+        }
     }
 }
--- src/utils/node_reader.rs
+++ src/utils/node_reader.rs
@@ -1,5 +1,7 @@
+use super::guard::FileGuard;
 use anyhow::{Result, anyhow};
 use compact_str::CompactString;
+use libc::{O_RDONLY, c_void, open, read};
 use likely_stable::unlikely;
 use std::{fs::File, io::Read};
 use stringzilla::sz;
@@ -8,7 +10,7 @@ use alloc::ffi::CString;
 
 pub fn read_file(file: &str) -> Result<CompactString> {
     let mut file = File::open(file)?;
-    let mut buffer = [0u8; 32];
+    let mut buffer = [0u8; 16];
     let _ = file.read(&mut buffer)?;
     let pos = sz::find(buffer, b"\n");
     let buffer = pos.map_or(&buffer[..], |pos| &buffer[..pos]);
@@ -17,28 +19,21 @@ pub fn read_file(file: &str) -> Result<CompactString> {
 }
 
 pub fn read_to_byte(file: &str) -> Result<[u8; 16]> {
-    #[cfg(debug_assertions)]
-    let start = minstant::Instant::now();
     let c_file = CString::new(file)?;
-    let fd = unsafe { libc::open(c_file.as_ptr(), libc::O_RDONLY) };
-    if unlikely(fd == -1) {
-        return Err(anyhow!("Cannot open file."));
-    }
-
     let mut buffer = [0u8; 16];
-    let bytes_read =
-        unsafe { libc::read(fd, buffer.as_mut_ptr().cast::<libc::c_void>(), buffer.len()) };
     unsafe {
-        libc::close(fd);
+        let fd = open(c_file.as_ptr(), O_RDONLY);
+        let _fd_guard = FileGuard::new(fd);
+        if unlikely(fd == -1) {
+            return Err(anyhow!("Cannot open file."));
+        }
+
+        let bytes_read = read(fd, buffer.as_mut_ptr().cast::<c_void>(), 16);
+
+        if unlikely(bytes_read == -1) {
+            return Err(anyhow!("Cannot read file."));
+        }
     }
 
-    if unlikely(bytes_read == -1) {
-        return Err(anyhow!("Cannot read file."));
-    }
-    #[cfg(debug_assertions)]
-    {
-        let end = start.elapsed();
-        log::debug!("comm时间: {:?}", end);
-    }
     Ok(buffer)
 }
