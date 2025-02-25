5c94dbc4e7547ce2bbc893ef99ae4a35b1d03564
reigadegr <272268642@qq.com>
2025-02-25 22:16:50 +0800

    perf: get_thread_cpu_time也避免format宏的使用

--- src/cpu_common/usage_tracker.rs
+++ src/cpu_common/usage_tracker.rs
@@ -1,9 +1,9 @@
-use crate::utils::node_reader::read_to_byte;
+use crate::utils::node_reader::read_to_byte_sp;
 use atoi::atoi;
 use libc::pid_t;
 use stringzilla::sz;
 extern crate alloc;
-use alloc::format;
+use std::io::Write;
 
 #[derive(Debug, Clone)]
 pub struct UsageTracker {
@@ -21,8 +21,14 @@ impl UsageTracker {
 }
 
 fn get_thread_cpu_time(tid: pid_t) -> u64 {
-    let stat_path = format!("/proc/{tid}/schedstat");
-    let buffer = read_to_byte::<32>(&stat_path).unwrap_or([0u8; 32]);
+    let mut stat_path = [0u8; 32];
+    stat_path[0..6].copy_from_slice(&b"/proc/"[..]);
+    if write!(&mut stat_path[6..], "{tid}/schedstat").is_err() {
+        return 0;
+    }
+
+    let buffer = read_to_byte_sp::<32>(&stat_path).unwrap_or([0u8; 32]);
+
     let pos = sz::find(buffer, b" ");
     let buffer = pos.map_or(&buffer[..], |pos| &buffer[..pos]);
     atoi::<u64>(buffer).unwrap_or(0)
