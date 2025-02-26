5a58cafc527110cfc7af0478cc704c7a2bec8023
reigadegr <272268642@qq.com>
2025-02-26 21:20:07 +0800

    Revert: 回退对line.split(|&b| b.is_ascii_whitespace())的修改

--- src/activity/get_top_pid.rs
+++ src/activity/get_top_pid.rs
@@ -16,7 +16,11 @@ impl TopPidInfo {
         let pid = dump
             .split(|&b| b == b'\n')
             .find(|line| sz::find(line, b" TOP").is_some())
-            .and_then(|line| line.split(|&b| b == b' ').filter(|&s| !s.is_empty()).nth(4))
+            .and_then(|line| {
+                line.split(|&b| b.is_ascii_whitespace())
+                    .filter(|&s| !s.is_empty())
+                    .nth(4)
+            })
             .and_then(atoi::<pid_t>)
             .unwrap_or_default();
         Self { pid }
