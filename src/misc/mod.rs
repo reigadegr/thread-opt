456d36eb493031260e2a9e3fab3bb827f4636f2a
reigadegr <272268642@qq.com>
2025-02-26 19:00:13 +0800

    chore: 避免不必要的类型转换; 精简Hashbrown的feature

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,21 +1,16 @@
 pub mod logger;
 use crate::{cgroup::group_info::print_group_core, utils::node_reader::write_to_byte};
-use anyhow::Result;
 use itoa::Buffer;
 use libc::getpid;
 use likely_stable::unlikely;
 use log::info;
 use logger::{init_log, log_metainfo};
 extern crate alloc;
-use alloc::ffi::CString;
 
 pub fn init_misc() {
     working_in_background();
     init_log();
-    let rs = set_main_thread_name("AffinitySetter");
-    if unlikely(rs.is_err()) {
-        info!("Cannot rename the main thread name.");
-    }
+    set_main_thread_name(b"AffinitySetter\0");
     log_metainfo();
     print_misc();
     print_group_core();
@@ -30,18 +25,16 @@ fn working_in_background() {
     }
 }
 
-fn set_main_thread_name(name: &str) -> Result<()> {
-    let truncated_name = if unlikely(name.len() > 15) {
+fn set_main_thread_name(name: &[u8]) {
+    let thread_name = if unlikely(name.len() > 15) {
         &name[..15]
     } else {
         name
     };
 
-    let thread_name = CString::new(truncated_name)?;
     unsafe {
         libc::pthread_setname_np(libc::pthread_self(), thread_name.as_ptr());
     }
-    Ok(())
 }
 
 fn print_misc() {
