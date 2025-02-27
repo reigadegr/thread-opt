f708fd4161e365582502a4b7c1c501caa8fae50d
reigadegr <272268642@qq.com>
2025-02-28 07:57:37 +0800

    build: 稳定升级至2024

--- debug.sh
+++ debug.sh
@@ -1,4 +1,2 @@
-sed -i 's/edition = "2021"/edition = "2024"/g' Cargo.toml
 cargo fmt
 cargo clippy
-sed -i 's/edition = "2024"/edition = "2021"/g' Cargo.toml
--- release.sh
+++ release.sh
@@ -1,6 +1,4 @@
-sed -i 's/edition = "2021"/edition = "2024"/g' Cargo.toml
 cargo fmt
-sed -i 's/edition = "2024"/edition = "2021"/g' Cargo.toml
 rm -rf output
 rm -rf $(find ./target/aarch64-linux-android/release -name "*thread-opt*")
 RUSTFLAGS="-C default-linker-libraries" python3 ./make.py build --release --nightly
