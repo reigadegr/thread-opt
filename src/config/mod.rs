92a79340202f4c762335b3eea3901a8caf65cd39
reigadegr <272268642@qq.com>
2025-03-01 00:41:11 +0800

    feat: 序列化为字节数组

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,7 +2,9 @@ use crate::utils::node_reader::read_file;
 use compact_str::CompactString;
 use serde::Deserialize;
 extern crate alloc;
-use alloc::{sync::Arc, vec::Vec};
+use alloc::{fmt::format, sync::Arc, vec::Vec};
+use anyhow::Result;
+use heapless::String;
 use once_cell::sync::{Lazy, OnceCell};
 
 pub static PROFILE: Lazy<Config> = Lazy::new(|| {
@@ -22,6 +24,22 @@ pub struct Config {
 #[derive(Deserialize)]
 pub struct NameMatch {
     pub packages: Vec<CompactString>,
+    pub policy: Policy,
+}
+
+type ByteArray = heapless::Vec<u8, 16>;
+#[derive(Deserialize)]
+pub struct Policy {
+    #[serde(deserialize_with = "deserialize_byte_array")]
+    pub top: Vec<ByteArray>,
+    #[serde(deserialize_with = "deserialize_byte_array")]
+    pub only6: Vec<ByteArray>,
+    #[serde(deserialize_with = "deserialize_byte_array")]
+    pub only7: Vec<ByteArray>,
+    #[serde(deserialize_with = "deserialize_byte_array")]
+    pub middle: Vec<ByteArray>,
+    #[serde(deserialize_with = "deserialize_byte_array")]
+    pub background: Vec<ByteArray>,
 }
 
 pub fn init_packages(vec: &[CompactString]) -> &'static [&'static str] {
@@ -41,4 +59,19 @@ pub fn init_packages(vec: &[CompactString]) -> &'static [&'static str] {
         Arc::from(static_slices.into_boxed_slice())
     });
     &cached[..]
+}
+
+fn deserialize_byte_array<'de, D>(deserializer: D) -> Result<Vec<heapless::Vec<u8, 16>>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let strings: Vec<CompactString> = Vec::deserialize(deserializer)?;
+    let mut result = Vec::new();
+    for s in strings {
+        let bytes = s.as_bytes();
+        let vec = heapless::Vec::from_slice(bytes)
+            .map_err(|e| serde::de::Error::custom("String exceeds capacity"))?;
+        result.push(vec);
+    }
+    Ok(result)
 }
