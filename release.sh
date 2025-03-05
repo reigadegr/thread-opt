9da166a2720c7beb1c68aaff4a2efde212d0e57d
reigadegr <272268642@qq.com>
2025-03-05 23:51:04 +0800

    build: 优化编译参数

--- release.sh
+++ release.sh
@@ -2,6 +2,11 @@ cargo fmt
 rm -rf output
 rm -rf $(find ./target/aarch64-linux-android/release -name "*thread-opt*")
 
-export RUSTFLAGS="-C default-linker-libraries -C link-args=-Wl,--gc-sections -C link-args=-Wl,--as-needed -C link-args=-Wl,--icf=all -C link-args=-Wl -C link-args=-Wl,--pack-dyn-relocs=android+relr -C link-args=-Wl,-x -C link-args=-Wl,-s -C link-args=-Wl,--strip-all"
+export RUSTFLAGS="
+-C default-linker-libraries \
+-C link-args=-fomit-frame-pointer \
+-C link-args=-Wl,--as-needed,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
+"
 
+# export RUSTFLAGS="-C default-linker-libraries"
 python3 ./make.py build --release --nightly
