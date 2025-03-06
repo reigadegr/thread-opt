9dbca54bafaf05c87e1e85a3171db71ba84e6df8
reigadegr <272268642@qq.com>
2025-03-06 21:42:21 +0800

    feat: 优化用户体验，close_core开启时自动转移only7到dualo

--- src/policy/name_match/cfg_start.rs
+++ src/policy/name_match/cfg_start.rs
@@ -1,14 +1,25 @@
 use super::common::Policy;
-use crate::{config, policy::pkg_cfg::StartArgs, utils::node_reader::write_to_byte};
+use crate::{
+    cgroup::group_info::get_top_group, config, policy::pkg_cfg::StartArgs,
+    utils::node_reader::write_to_byte,
+};
 extern crate alloc;
+use alloc::boxed::Box;
+
 pub fn start_task(args: &mut StartArgs<'_>, policy: &config::Policy) {
-    if policy.core_closer {
+    let mut dualo = &policy.dualo;
+    let mut only7 = &policy.only7;
+    let empty_box: Box<[heapless::Vec<u8, 16>]> = Box::new([]);
+
+    if policy.core_closer && get_top_group().len() > 1 {
         let _ = write_to_byte(b"/sys/devices/system/cpu/cpu7/online\0", b"0");
+        dualo = &policy.only7;
+        only7 = &empty_box;
     }
     let policy = Policy {
         top: &policy.top,
-        dualo: &policy.dualo,
-        only7: &policy.only7,
+        dualo,
+        only7,
         middle: &policy.middle,
         mono: &policy.mono,
         background: &policy.background,
--- src/policy/usage_top1/cfg_start.rs
+++ src/policy/usage_top1/cfg_start.rs
@@ -1,5 +1,11 @@
 use super::common::{CmdType, Policy};
-use crate::{config, policy::pkg_cfg::StartArgs, utils::node_reader::write_to_byte};
+use crate::{
+    cgroup::group_info::get_top_group, config, policy::pkg_cfg::StartArgs,
+    utils::node_reader::write_to_byte,
+};
+
+extern crate alloc;
+use alloc::boxed::Box;
 
 pub fn start_task(
     args: &mut StartArgs<'_>,
@@ -7,13 +13,19 @@ pub fn start_task(
     comm_prefix: &[u8],
     cmd_type: &CmdType,
 ) {
-    if policy.core_closer {
+    let mut dualo = &policy.dualo;
+    let mut only7 = &policy.only7;
+    let empty_box: Box<[heapless::Vec<u8, 16>]> = Box::new([]);
+    if policy.core_closer && get_top_group().len() > 1 {
         let _ = write_to_byte(b"/sys/devices/system/cpu/cpu7/online\0", b"0");
+        dualo = &policy.only7;
+        only7 = &empty_box;
     }
+
     let policy = Policy {
         top: &policy.top,
-        dualo: &policy.dualo,
-        only7: &policy.only7,
+        dualo,
+        only7,
         middle: &policy.middle,
         mono: &policy.mono,
         background: &policy.background,
