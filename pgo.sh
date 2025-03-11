7d7fc0d7619e165647ba90245d7ceeadadc94a21
reigadegr <272268642@qq.com>
2025-03-11 16:56:34 +0800

    build: 优化编译参数

--- pgo.sh
+++ pgo.sh
@@ -16,8 +16,17 @@ if [ ! -z $(which dumpsys) ]; then
 fi
 
 RUSTFLAGS="-C default-linker-libraries \
- -C profile-use=$pgo_path/merged.profdata \
- -C link-args=-fomit-frame-pointer \
--C link-args=-Wl,--as-needed,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
+-Z external-clangrt \
+-Z macro-backtrace \
+-Z remap-cwd-prefix=. \
+-Z dep-info-omit-d-target \
+-C target-feature=xeon \
+-C llvm-args=--enable-ml-inliner=release \
+-C llvm-args=-inliner-interactive-include-default \
+-C llvm-args=-ml-inliner-model-selector=arm64-mixed \
+-C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
+-C profile-use=$pgo_path/merged.profdata \
+-C link-args=-fomit-frame-pointer \
+-C link-args=-Wl,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
 " python3 ./make.py build --release --nightly -v
 # cargo +nightly ndk -p 35 -t arm64-v8a build --target aarch64-linux-android -Z trim-paths -Z build-std --release  --
