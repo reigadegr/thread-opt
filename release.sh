2e125af16d2facd1928d02cd8af735c43272b722
reigadegr <272268642@qq.com>
2025-05-08 22:47:06 +0800

    fix: 避免线程名更新未被同步识别; 试图修复异常退出，虽然概率低

--- release.sh
+++ release.sh
@@ -7,13 +7,19 @@ export RUSTFLAGS="-C default-linker-libraries \
 -Z macro-backtrace \
 -Z remap-cwd-prefix=. \
 -Z dep-info-omit-d-target \
--C target-feature=xeon \
 -C llvm-args=-enable-ml-inliner=release \
 -C llvm-args=-inliner-interactive-include-default \
 -C llvm-args=-ml-inliner-model-selector=arm64-mixed \
 -C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
 -C link-args=-fomit-frame-pointer \
--C link-args=-Wl,--icf=all,-z,relro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
+-C link-args=-static-libgcc \
+-C link-args=-static-libstdc++ \
+-C llvm-args=-mergefunc-use-aliases \
+-C llvm-args=-enable-shrink-wrap=1 \
+-C llvm-args=-enable-gvn-hoist \
+-C llvm-args=-enable-loop-versioning-licm \
+-C link-args=-Wl,-O3,--gc-sections,--as-needed \
+-C link-args=-Wl,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
 " 
 
 # export RUSTFLAGS="-C default-linker-libraries"
