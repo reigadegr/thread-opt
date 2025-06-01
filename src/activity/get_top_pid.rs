9794a5ce8c25381d64f2f132851211073fbab863
reigadegr <272268642@qq.com>
2025-06-01 10:20:08 +0000

    refactor: 使用window visible-apps进行解析顶层pid

--- src/activity/get_top_pid.rs
+++ src/activity/get_top_pid.rs
@@ -14,11 +14,18 @@ pub struct TopPidInfo {
 
 impl TopPidInfo {
     pub fn new(dump: &[u8]) -> Self {
-        let pid = dump
-            .sz_splits(&b"\n")
-            .find(|line| sz::find(line, b" TOP").is_some())
+        let multi_window = sz::find(dump, b"Window #1").is_some();
+
+        let pid = if multi_window {
+            dump.sz_rsplits(&b"\n")
+                .find(|line| sz::find(line, b"Session{").is_some())
+        } else {
+            dump.sz_splits(&b"\n")
+                .find(|line| sz::find(line, b"Session{").is_some())
+        };
+        let pid = pid
             .and_then_likely(|line| {
-                line.sz_rfind(b"/").and_then_likely(|pos1| {
+                line.sz_rfind(b":").and_then_likely(|pos1| {
                     line[..pos1].sz_rfind(b" ").map(|pos2| &line[pos2 + 1..])
                 })
             })
@@ -42,7 +49,7 @@ impl TopAppUtils {
             .unwrap();
 
         let dumper = loop {
-            match Dumpsys::new("activity") {
+            match Dumpsys::new("window") {
                 Some(d) => break d,
                 None => sleep_secs(1),
             }
@@ -59,14 +66,13 @@ impl TopAppUtils {
             match self.inotify.read_events_blocking(&mut [0; 1024]) {
                 Ok(_) => break,
                 Err(e) => {
-                    info!("Failed to read_events: {e}, retrying");
+                    info!("Failed to read events: {e}, retrying");
                     sleep_secs(1);
                 }
             }
         }
-
         let dump = loop {
-            match self.dumper.dump_to_byte::<1024>(&["lru"]) {
+            match self.dumper.dump_to_byte::<32768>(&["visible-apps"]) {
                 Ok(dump) => break dump,
                 Err(e) => {
                     info!("Failed to dump windows: {e}, retrying");
@@ -74,7 +80,6 @@ impl TopAppUtils {
                 }
             }
         };
-
         TopPidInfo::new(&dump)
     }
 }
