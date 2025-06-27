b7db6827e954070d0647f39aa09f8677cb0fa7e6
reigadegr <272268642@qq.com>
2025-06-27 23:21:46 +0000

    chore: 移除no_main/no_std，开启thp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![no_std]
-#![no_main]
 #![warn(clippy::nursery, clippy::pedantic)]
 #![allow(
     clippy::non_std_lazy_statics,
@@ -17,24 +15,13 @@ mod policy;
 mod scheduler;
 mod utils;
 
-use core::{ffi::CStr, slice};
-use libc::{c_char, c_int};
 use misc::init_misc;
 use scheduler::Scheduler;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-#[unsafe(no_mangle)]
-unsafe extern "C" fn main(argc: c_int, argv: *const *const c_char) {
+fn main() {
     init_misc();
-    let args = unsafe { slice::from_raw_parts(argv, argc.try_into().unwrap_or(0)) };
-
-    for arg in args {
-        if let Ok(s) = unsafe { CStr::from_ptr(*arg) }.to_str() {
-            #[cfg(debug_assertions)]
-            log::info!("命令行参数{s}");
-        }
-    }
     Scheduler::new().start_run();
 }
