189ce21aeb52b1be38107b3dee59fdd46b6e6e33
reigadegr <272268642@qq.com>
2025-08-10 08:53:09 +0000

    perf: 继续优化编译参数

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ export RUSTFLAGS="
     -Z remap-cwd-prefix=. \
     -Z dep-info-omit-d-target \
     -Z merge-functions=aliases \
+    -C llvm-args=-aggressive-ext-opt \
     -C llvm-args=-enable-ml-inliner=release \
     -C llvm-args=-regalloc-enable-advisor=release \
     -C llvm-args=-hot-cold-split=true \
@@ -17,13 +18,13 @@ export RUSTFLAGS="
     -C llvm-args=-inliner-interactive-include-default \
     -C llvm-args=-ml-inliner-model-selector=arm64-mixed \
     -C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
-    -C link-args=-fomit-frame-pointer \
     -C llvm-args=-mergefunc-use-aliases \
     -C llvm-args=-enable-shrink-wrap=1 \
     -C llvm-args=-enable-gvn-hoist \
     -C llvm-args=-enable-loop-versioning-licm \
+    -C link-args=-fomit-frame-pointer \
     -C link-args=-Wl,-O3,--gc-sections,--as-needed \
-    -C link-args=-Wl,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,-z,now
+    -C link-args=-Wl,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr,-x,-s,--strip-all,--relax
 " 
 
 # export RUSTFLAGS="-C default-linker-libraries"
