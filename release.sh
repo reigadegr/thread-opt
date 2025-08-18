b2f70d942db66fd6176ef8f21978e7fd9ab56e62
reigadegr <2722688642@qq.com>
2025-08-18 20:40:06 +0000

    chore: 更稳定的编译参数

--- release.sh
+++ release.sh
@@ -5,25 +5,21 @@ rm -rf $(find ./target/aarch64-linux-android/release -name "*thread-opt*")
 export RUSTFLAGS="
     -Z validate-mir \
     -Z verify-llvm-ir \
-    -Z mir-opt-level=2 \
+    -Z mir-opt-level=1 \
     -Z share-generics=yes \
     -Z remap-cwd-prefix=. \
     -Z function-sections=yes \
     -Z dep-info-omit-d-target \
     -C default-linker-libraries \
     -C symbol-mangling-version=v0 \
-    -C llvm-args=-vectorize-loops \
     -C llvm-args=-enable-misched \
-    -C llvm-args=-enable-branch-hint \
     -C llvm-args=-enable-post-misched \
     -C llvm-args=-enable-dfa-jump-thread \
     -C link-args=-fomit-frame-pointer \
     -C link-arg=-Wl,--no-rosegment \
-    -C link-args=-Wl,-O3,--gc-sections,--as-needed \
-    -C link-args=-Wl,-z,norelro,-x,-z,noexecstack,--pack-dyn-relocs=android+relr,-s,--strip-all,--relax \
-    -C llvm-args=-enable-ml-inliner=release \
-    -C llvm-args=-ml-inliner-skip-policy=if-caller-not-cold \
-    -C llvm-args=-ml-inliner-model-selector=arm64-mixed
+    -C link-arg=-Wl,--sort-section=alignment \
+    -C link-args=-Wl,-O1,--gc-sections,--as-needed \
+    -C link-args=-Wl,-z,norelro,-x,-z,noexecstack,--pack-dyn-relocs=android+relr,-s,--strip-all,--relax
 " 
 
 export RUSTFLAGS="$RUSTFLAGS -Z time-passes"
