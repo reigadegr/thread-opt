e46e5fffd2928bb285b4beb7d8860545f0828dd4
reigadegr <2722688642@qq.com>
2025-08-27 10:31:44 +0800

    chore: 改编译参数

--- release.sh
+++ release.sh
@@ -3,23 +3,33 @@ rm -rf output
 rm -rf $(find ./target/aarch64-linux-android/release -name "*thread-opt*")
 
 export RUSTFLAGS="
-    -Z validate-mir \
-    -Z verify-llvm-ir \
     -Z mir-opt-level=2 \
+    -Z dylib-lto=yes \
+    -Z inline-mir=yes \
+    -Z fewer-names=yes \
+    -Z box-noalias=yes \
     -Z share-generics=yes \
     -Z remap-cwd-prefix=. \
+    -Z mutable-noalias=yes \
     -Z function-sections=yes \
     -Z dep-info-omit-d-target \
+    -Z flatten-format-args=yes \
+    -Z saturating-float-casts=yes \
+    -Z mir-enable-passes=+Inline \
+    -Z precise-enum-drop-elaboration=yes \
     -C default-linker-libraries \
+    -C relro-level=none \
+    -C code-model=small \
+    -C relocation-model=pie \
     -C symbol-mangling-version=v0 \
+    -C llvm-args=-fp-contract=off \
     -C llvm-args=-enable-misched \
     -C llvm-args=-enable-post-misched \
     -C llvm-args=-enable-dfa-jump-thread \
-    -C link-args=-fomit-frame-pointer \
     -C link-arg=-Wl,--no-rosegment \
     -C link-arg=-Wl,--sort-section=alignment \
-    -C link-args=-Wl,-O1,--gc-sections,--as-needed \
-    -C link-args=-Wl,-z,norelro,-x,-z,noexecstack,--pack-dyn-relocs=android+relr,-s,--strip-all,--relax
+    -C link-args=-Wl,-O3,--gc-sections,--as-needed \
+    -C link-args=-Wl,-x,-z,noexecstack,--pack-dyn-relocs=android+relr,-s,--strip-all,--relax
 " 
 
 export RUSTFLAGS="$RUSTFLAGS -Z time-passes"
