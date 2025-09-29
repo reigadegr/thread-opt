66481e3b5d5c2b3a4ebd6b6ed350a0cfd9f33381
reigadegr <2722688642@qq.com>
2025-09-29 11:40:23 +0800

    fix: 无法读文件

--- src/utils/node_reader.rs
+++ src/utils/node_reader.rs
@@ -3,8 +3,9 @@ use anyhow::{Result, anyhow};
 use compact_str::CompactString;
 use core::ptr::copy_nonoverlapping;
 use itoa::Buffer;
-use libc::{O_CREAT, O_RDONLY, O_TRUNC, O_WRONLY, c_void, chmod, chown, open, pid_t, read, write};
+use libc::{O_CREAT, O_TRUNC, O_WRONLY, c_void, chmod, chown, open, pid_t, write};
 use likely_stable::unlikely;
+use std::{fs::File, io::Read, str::from_utf8};
 use stringzilla::sz;
 
 pub fn read_file<const N: usize>(file: &[u8]) -> Result<CompactString> {
@@ -16,19 +17,27 @@ pub fn read_file<const N: usize>(file: &[u8]) -> Result<CompactString> {
 }
 
 pub fn read_to_byte<const N: usize>(file: &[u8]) -> Result<[u8; N]> {
-    let mut buffer = [0u8; N];
-    unsafe {
-        let fd = open(file.as_ptr(), O_RDONLY);
-        if unlikely(fd == -1) {
-            return Err(anyhow!("Cannot open file."));
-        }
-        let _fd_guard = FileGuard::new(fd);
-        let bytes_read = read(fd, buffer.as_mut_ptr().cast::<c_void>(), N);
+    let end = sz::find(file, b"\0").unwrap_or(N);
+    let file = &file[..end];
+    let file = from_utf8(file)?;
 
-        if unlikely(bytes_read == -1) {
+    let Ok(mut file) = File::open(file) else {
+        return Err(anyhow!("Cannot open file."));
+    };
+    let mut buffer = [0u8; N];
+    let mut total_read = 0;
+    while total_read < N {
+        let Ok(n) = file.read(&mut buffer) else {
             return Err(anyhow!("Cannot read file."));
+        };
+
+        if n == 0 {
+            break;
         }
+
+        total_read += n;
     }
+
     Ok(buffer)
 }
 
